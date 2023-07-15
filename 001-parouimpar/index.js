41c25eff79b3b6e030333c06c39f39e8543d3163
O Jesas <jesaiassa@gmail.com>
2023-07-15 13:22:46 -0300

    Finish Functions

--- 001-parouimpar/index.js
+++ 001-parouimpar/index.js
@@ -1,4 +1,5 @@
 //Par ou Impar
+
 let numeros = [];
 for (let i = 0; i <= 33; i++) { numeros[i] = i; }
 
@@ -18,24 +19,16 @@ function par_impar(numeros) {
         }
     }
     process.stdout.write(`Foi fornecida uma lista com ${numeros.length} números, a seguir: `);
-    for (let n of numeros) { process.stdout.write(`\x1b[31m${n} \x1b[0m`); }
+    for (let n of numeros) { process.stdout.write(`${n} `); }
     console.log("");
     process.stdout.write(`Com ${npares} números pares, a seguir: `);
-    for (let par of pares) { process.stdout.write(`\x1b[32m${par} \x1b[0m`); }
+    for (let par of pares) { process.stdout.write(`${par} `); }
     console.log(``);
     process.stdout.write(`E ${nimpares} números impares, a seguir: `);
-    for (let impar of impares) { process.stdout.write(`\x1b[33m${impar} \x1b[0m`); }
+    for (let impar of impares) { process.stdout.write(`${impar} `); }
     console.log(``);
 
-    
-    /*console.log('\x1b[31m%s\x1b[0m', 'Texto vermelho');
-    console.log('\x1b[32m%s\x1b[0m', 'Texto verde');
-    console.log('\x1b[33m%s\x1b[0m', 'Texto amarelo');
-
-
-    process.stdout.write(`\x1b[31mTexto vermelho\x1b[0m`);
-    process.stdout.write('\x1b[32mTexto verde\x1b[0m');
-    process.stdout.write('\x1b[33mTexto amarelo\x1b[0m');*/
 
 }
 par_impar(numeros);
+
