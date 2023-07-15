4e647c496df46ef8de5dfd293fb02d67d9c31799
O Jesas <jesaiassa@gmail.com>
2023-07-15 14:12:10 -0300

    add js file for codes test

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+const colors = require('colors');
+
+console.log(colors.red('Texto em vermelho'));
+console.log(colors.bgBlue.white('Texto com fundo azul e cor branca'));
+console.log(colors.bold.underline('Texto em negrito e sublinhado'));
+
+
+console.log(colors.red('Texto em vermelho'));
+console.log(colors.bgBlue.white('Texto com fundo azul e cor branca'));
+console.log(colors.bold.underline('Texto em negrito e sublinhado'));
+console.log(colors.blue('Texto em azul'));
+process.stdout.write(colors.blue('Texto em azul'));
